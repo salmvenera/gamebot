00f79d555273f2de1beb30cae49ef1f6eb3f60d4
salmvenera <ahmetovavm@gmail.com>
2021-10-10 10:11:20 +0500

    исправл игра

--- script.js
+++ script.js
@@ -1,39 +1,51 @@
 'use strict';
 
-let randomNum = parseInt(Math.random() * 100);  //замкнутая переменная
+  let randomNum = parseInt(Math.random() * 100);  //замкнутая переменная
 
 function isNum(num) {
   return !isNaN(parseFloat(num)) && isFinite(num);
 }
 
 function guessNum(serNum) {
-
-     function guess() {
-      let userNum = prompt('Угадай число от 1 до 100'); 
-
-      if (!isNum(userNum)) {
-        userNum = +userNum;
-      }
-
-      else if (userNum > randomNum) {
-      alert('Загаданное число меньше');
-       return guess(userNum)
-      } 
-      else if (userNum < randomNum) {
-      alert('Загаданное число больше');
-      return guess(userNum)
-      }
-      else if (userNum !== Number) {               
-      alert('Поздравляю, Вы угадали!!!');
-      alert('Игра окончена')
-      return
-      }
-      else if (userNum === "null" || userNum === null || userNum === "" );  {        //получается
-      alert('Введи число!');
-      } return guess(userNum)
     
+function guess() {
+  let userNum = prompt('Угадай число от 1 до 100', ''); 
+
+ if (userNum === null) {
+   alert(`Досвидос!!!`);
+  } 
+
+ if (isNaN(userNum)) {
+   alert('Введи число!');
+ return guess(userNum)
   }
-    guess()
+
+ if (userNum === null) {
+ return
+  }
+
+ if (!isNum(userNum)) {
+   userNum = +userNum;
+  }
+
+ else if (userNum > randomNum) {
+   alert('Загаданное число меньше');
+ return guess(userNum)
+  } 
+
+ else if (userNum < randomNum) {
+   alert('Загаданное число больше');
+ return guess(userNum)
+ }
+
+ else if (userNum !== Number) {               
+   alert('Поздравляю, Вы угадали!!!');
+   alert('Игра окончена')
+ return
+ }
+     
+}
+guess()
 }
 guessNum();
 console.log(randomNum);
